96db4cad39d48bc0b39d49e3cdcaeaf46503ddb4
Miles Steele <miles@toast.local>
2011-01-09 00:44:33 -0500

    bucket, update

--- cake.py
+++ cake.py
@@ -4,27 +4,32 @@ import time
 
 class c_master:
 	def __init__(self):
-		pass
+		self.bucket = []
+		self.bucklen = 0
 	
 	def make_node(self, i_mass, i_pos, i_vel=numpy.zeros(2)):
 		newnode = c_node(self, i_mass, i_pos, i_vel)
+		self.bucket.append(newnode)
+		self.bucklen = len(self.bucket)
 		return newnode
 	
-	def update():
-		pass
+	def update(self):
+		for i in range(0,self.bucklen):
+			self.bucket[i].update()
 
 class c_node:
 	def __init__(self, i_master, i_mass, i_pos, i_vel):
+		# note: not integer resistant
 		self.master = i_master
 		self.pos = i_pos
 		self.vel = i_vel
 		self.accel = numpy.zeros(2)
 		self.mass = i_mass
 	
-	def push(force):
+	def push(self, force):
 		self.accel += force/self.mass
 	
-	def update():
+	def update(self):
 		self.vel += self.accel
 		self.pos += self.vel
 		self.accel = numpy.zeros(2)
--- example1.py
+++ example1.py
@@ -1,12 +1,19 @@
 import numpy
 import random
 import cake
+import time
 
 master = cake.c_master()
-na = master.make_node(1, numpy.array([0, 0]), i_vel=numpy.array([1, 0]))
-nb = master.make_node(1, numpy.array([5, 0]))
+na = master.make_node(1, numpy.array([0., 0.]), i_vel=numpy.array([0.01, 0.]))
+nb = master.make_node(1, numpy.array([5., 0.]))
 
 frame = -1
 while True:
 	frame += 1
 	
+	master.update()
+	
+	print "\n" + str(frame)
+	print na.pos
+	print nb.pos
+	time.sleep(.1)
